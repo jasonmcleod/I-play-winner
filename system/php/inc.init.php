1d7302bde05510e1a8f9038febd03269c9847dd0
Jason McLeod <pixelparty@gmail.com>
2011-07-20 15:04:55 -0400

    renamed some files and db tables to be a little more agnostic. also hooked up the json feed of apps to build the app library

--- system/php/inc.init.php
+++ system/php/inc.init.php
@@ -1,28 +1,28 @@
 <?
     error_reporting(E_ALL);
     ini_set('show_errors','on');
-    
+
     $hostname_emu = "localhost";
-    $database_emu = "emulators";
+    $database_emu = "iplaywinner";
     $username_emu = "root";
-    $password_emu = "root";		
-    
+    $password_emu = "root";
+
     $emu = mysql_pconnect($hostname_emu, $username_emu, $password_emu) or trigger_error(mysql_error(),E_USER_ERROR);
-     
+
     function query($query,$debug=false) {
          if($debug) {
-             echo "<pre>$query</pre>";   
+             echo "<pre>$query</pre>";
          }
-         
+
          global $database_emu, $emu;
          $return = array();
-         
+
          mysql_select_db($database_emu, $emu);
          $thisQuery = mysql_query($query, $emu) or die(mysql_error());
          $return['total'] = mysql_num_rows($thisQuery);
          $return['data'] = mysql_fetch_assoc($thisQuery);
          $return['object'] = $thisQuery;
-         
+
          return $return;
      }
 ?>
